daf8c09140d992cd5e48be3f2c071cf6782c3929
Deliay <admin@remiliascarlet.com>
2023-05-12 10:22:54 +0800

    adjust structure to support pluginable GUI architecture

---
+++ Dota2Modding.VisualEditor.GUI.Abstraction/MainWindowConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2Modding.VisualEditor.GUI.Abstraction
+{
+    public class MainWindowConfiguration
+    {
+        public float Width { get; set; }
+        public float Height { get; set; }
+    }
+}
