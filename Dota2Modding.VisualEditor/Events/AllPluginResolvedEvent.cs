064be5144c5fad467f981edc32759b7b42453ae3
Deliay <admin@remiliascarlet.com>
2023-05-12 10:15:34 +0800

    adjust structure to support pluginable GUI architecture

---
+++ Dota2Modding.VisualEditor/Events/AllPluginResolvedEvent.cs
@@ -0,0 +1,13 @@
+﻿using EmberKernel.Services.EventBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2Modding.VisualEditor.Events
+{
+    public class AllPluginResolvedEvent : Event<AllPluginResolvedEvent>
+    {
+    }
+}
