e5e19ecc973a45224723dd0f56e73438a8f148f3
Deliay <admin@remiliascarlet.com>
2023-06-06 00:06:14 +0800

    partial editor viewmodel

---
+++ Dota2Modding.VisualEditor.Plugins.Project/ViewModel/DotaHeroesEditorManager.cs
@@ -0,0 +1,19 @@
+﻿using Dota2Modding.Common.Models.GameStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2Modding.VisualEditor.Plugins.Project.ViewModel
+{
+    public class DotaHeroesEditorManager
+    {
+        private readonly Packages packages;
+
+        public DotaHeroesEditorManager(Packages packages)
+        {
+            this.packages = packages;
+        }
+    }
+}
